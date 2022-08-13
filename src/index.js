27fb773faabeb117a6324d98f39a3055be9f22f9
Oliwia <Oliweirdo@gmail.com>
2022-08-13 23:37:59 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import{React} from 'react';
+import { ReactDOM } from 'react';
+
+
+function layout(){
+    return(
+        <div className="Layout">
+            <CharacterSheet/>
+        </div>
+    )
+}
+
+module.hot.accetpt();
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+
+module.exports= {
+    entry: './src/index.js',
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test:/\.scss$/,
+                use:[{
+                    loader:'style-loader',
+                },{
+                    loader:'css-loader',
+                },{
+                    loader:'sass-loader',
+                }],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: { presets: ['@babel/env','@babel/preset-react'] },
+              },
+           
+        ]
+    },
+    resolve:{
+        extensions: ['*','.js','.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        static: './'
+      }
+      
+}
